11c08dedb560aed38dbc98d678ae7885745e511b
sumit adep <83160210+sumitadep002@users.noreply.github.com>
2022-07-28 01:41:17 +0530

    Add files via upload

---
+++ GPIO/GPIO.cpp
@@ -0,0 +1,48 @@
+﻿void pinMode(uint8_t pin,bool Mode)
+{
+	if (pin<=7)
+	{
+	if(Mode==OUTPUT)
+	DDRD|=(1<<pin);
+	else if(Mode==INPUT)
+	DDRD&=(~(1<<pin));
+	}
+	else
+	{
+		if(Mode==OUTPUT)
+		DDRB|=(1<<pin);
+		else if(Mode==INPUT)
+		DDRB&=(~(1<<pin));
+	}
+	
+}
+
+void digitalWrite(uint8_t pin,bool Mode)
+{
+	if (pin<=7)
+	{
+		if(Mode==OUTPUT)
+		PORTD|=(1<<pin);
+		else if(Mode==INPUT)
+		DDRD&=(~(1<<pin));
+	}
+	else
+	{
+		if(Mode==OUTPUT)
+		PORTD|=(1<<pin);
+		else if(Mode==INPUT)
+		PORTD&=(~(1<<pin));
+	}
+}
+
+bool digitalRead(uint8_t pin)
+{
+	if(pin<=7)
+	{
+		return (PIND&(1<<pin));
+	}
+	else
+	{
+		return (PINB&(1<<pin));
+	}
+}
---
+++ GPIO/GPIO.h
@@ -0,0 +1,10 @@
+void pinMode(uint8_t,bool);
+void digitalWrite(uint8_t,bool);
+bool digitalRead(uint8_t);
+#define OUTPUT 1
+#define INPUT  0
+
+#define HIGH 1
+#define LOW  0
+
+#include "GPIO.cpp"
